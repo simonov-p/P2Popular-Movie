5eebdbf083d4344c16372b3e53cac4c0000d90b3
petr <simonov-p@ya.ru>
2015-09-13 12:06:21 +0300

    datail fragment: layout

---
+++ app/src/main/java/com/example/petr/udacitypopularmovies/Utility.java
@@ -0,0 +1,10 @@
+package com.example.petr.udacitypopularmovies;
+
+/**
+ * Created by petr on 13.09.2015.
+ */
+public class Utility {
+    public static String getReleaseYear(String release) {
+        return release.substring(0,4);
+    }
+}
--- app/src/main/java/com/example/petr/udacitypopularmovies/fragments/DetailFragment.java
+++ app/src/main/java/com/example/petr/udacitypopularmovies/fragments/DetailFragment.java
@@ -7,10 +7,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.petr.udacitypopularmovies.R;
+import com.example.petr.udacitypopularmovies.Utility;
 import com.example.petr.udacitypopularmovies.objects.Movie;
 import com.squareup.picasso.Picasso;
 
@@ -25,17 +27,26 @@ public class DetailFragment extends Fragment {
         Intent intent = getActivity().getIntent();
 
         if (intent != null) {
-            int position = intent.getIntExtra(GridFragment.EXTRA,-1);
+            int position = intent.getIntExtra(GridFragment.EXTRA, -1);
             Movie movie = GridFragment.mAdapter.getItem(position);
             Log.e("mytag3", movie.toString());
 
             TextView title = (TextView) rootView.findViewById(R.id.detail_title_text_view);
-            TextView overview = (TextView) rootView.findViewById(R.id.detail_description_text_view);
             ImageView poster = (ImageView) rootView.findViewById(R.id.detail_image_view);
+            TextView release = (TextView) rootView.findViewById(R.id.detail_release_text_view);
+            TextView duration = (TextView) rootView.findViewById(R.id.detail_duration_text_view);
+            TextView vote = (TextView) rootView.findViewById(R.id.detail_vote_average_text_view);
+            Button buttonFavorite = (Button) rootView.findViewById(R.id.detail_favorite_button);
+            TextView overview = (TextView) rootView.findViewById(R.id.detail_overview_text_view);
+
 
             title.setText(movie.title);
-            overview.setText(movie.overview);
             Picasso.with(getActivity()).load(movie.poster_path).into(poster);
+            release.setText(Utility.getReleaseYear(movie.release_date));
+            duration.setText("no dur");
+            vote.setText(String.format(getString(R.string.vote), movie.vote_average));
+            overview.setText(movie.overview);
+
         }
 
         return rootView;
